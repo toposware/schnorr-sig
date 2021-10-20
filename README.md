da58b76b7f919f842c26c2aa691fc23f2b65a631
Robin Salen <salenrobin@gmail.com>
2021-10-20 20:45:20 +0900

    Add documentation and update README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Schnorr-sig
-An export of the Schnorr signature implementation for use with the Winterfell library.
+
+This crate provides an ongoing implementation of a modified version of the Schnorr signature protocol, for efficient verification in a STARK AIR program.
+The underlying curve is currently the one originally discovered by STARKWARE, based on the Prime Field Fp with p = 2<sup>251</sup> + 17(2<sup>192</sup>) + 1, and curve equation E(Fp): y<sup>2</sup> = x<sup>3</sup> + x + B, with 
+B = `0x6f21413efbe40de150e596d72f7a8c5609ad26c15c915c1f4cdfcb99cee9e89`,
+and implemented [here](https://github.com/ToposWare/stark-curve).
+
+* This implementation does not require the Rust standard library
+
+## Description
+
+See :
+- [here](https://en.wikipedia.org/wiki/Schnorr_signature) for an introduction to Schnorr signatures,
+- [here](https://github.com/ToposWare/stark-curve) for the implementation of the underlying fields and elliptic curve,
+- [here](https://github.com/ToposWare/hash) for the implementation of the internal Rescue hash function.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,89 @@
 //! This crate provides an implementation of a custom Schnorr
 //! signature with internal Rescue hash on a 252-bit field.
 
+//! # Usage
+//!
+//! To generate a new random key pair, consisting of a secret scalar
+//! (private key) and an associated curve point in projective coordinate
+//! (public key), do as the following:
+//!
+//! ```rust
+//! use schnorr_sig::KeyPair;
+//! use rand_core::OsRng;
+//!
+//! let mut rng = OsRng;
+//! let new_key_pair = KeyPair::new(&mut rng);
+//! ```
+//!
+//! To sign a `message` seen as an array of `FieldElement` values, with a given
+//! private key `skey` and given source of randomness `rng`, either call the sign
+//! method from the `Signature` struct or from the private key directly, as shown
+//! in the following examples:
+//!
+//! ```rust
+//! use stark_curve::FieldElement;
+//! use schnorr_sig::PrivateKey;
+//! use schnorr_sig::Signature;
+//! use rand_core::OsRng;
+//!
+//! let mut rng = OsRng;
+//! let message = [FieldElement::one(); 42];
+//! let skey = PrivateKey::new(&mut rng);
+//!
+//! let signature = Signature::sign(&message, &skey, &mut rng);
+//! ```
+//!
+//! ```rust
+//! use stark_curve::FieldElement;
+//! use schnorr_sig::PrivateKey;
+//! use rand_core::OsRng;
+//!
+//! let mut rng = OsRng;
+//! let message = [FieldElement::one(); 42];
+//! let skey = PrivateKey::new(&mut rng);
+//!
+//! let signature = skey.sign(&message, &mut rng);
+//! ```
+//!
+//! To verify a signature against a given `message`, with a provided
+//! public key `pkey`, you can call the verify method from the `signature`
+//! directly, as shown in the following example:
+//!
+//! ```rust
+//! use stark_curve::FieldElement;
+//! use schnorr_sig::PrivateKey;
+//! use schnorr_sig::PublicKey;
+//! use rand_core::OsRng;
+//!
+//! let mut rng = OsRng;
+//! let message = [FieldElement::one(); 42];
+//! let skey = PrivateKey::new(&mut rng);
+//! let pkey = PublicKey::from_private_key(skey);
+//!
+//! let signature = skey.sign(&message, &mut rng);
+//!
+//! assert!(signature.verify(&message, &pkey).is_ok());
+//! ```
+//!
+//! You can also verify a signature directly from the
+//! `verify_signature` method implemented for the
+//! `PublicKey` and `KeyPair` types:
+//!
+//!
+//! ```rust
+//! use stark_curve::FieldElement;
+//! use schnorr_sig::KeyPair;
+//! use rand_core::OsRng;
+//!
+//! let mut rng = OsRng;
+//! let message = [FieldElement::one(); 42];
+//! let key_pair = KeyPair::new(&mut rng);
+//!
+//! let signature = key_pair.sign(&message, &mut rng);
+//!
+//! assert!(key_pair.verify_signature(&signature, &message).is_ok());
+//! ```
+
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![deny(broken_intra_doc_links)]
 #![deny(missing_debug_implementations)]
